199f68244f69b7df0649a417ee3c7b170daf3f77
Georg Grab <talkdirty@users.noreply.github.com>
2017-02-23 18:57:24 +0100

    Refactored nginx_reload

--- nginx_reload.sh
+++ nginx_reload.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-echo "Generating config for production site"
-export CHORSERV_IPS=$(dig +short chorserv-server-site-bundle | sed 's/\(.*\)/server \1:8000;/')
-export CHORSERV_DOMAIN=chorknaben-biberach.de
-envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/production-site.conf
+function create {
+	echo "Generating config for (docker-compose name $1, domain $2, internal port $3)"
+	export CHORSERV_IPS=$(dig +short $1 | sed "s/\(.*\)/server \1:$3;/")
+	export CHORSERV_DOMAIN=$2
+	if [ ! -z "$CHORSERV_IPS" ]; then
+		envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/$2.conf
+	else
+		echo "Will not create config for site; no upstreams"
+	fi
+}
 
-echo "Generating config for production backend"
-export CHORSERV_IPS=$(dig +short chr-cms | sed 's/\(.*\)/server \1:8002;/')            
-export CHORSERV_DOMAIN=backend.chorknaben-biberach.de
-envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/production-backend.conf
+create chorserv-server-site-bundle chorknaben-biberach.de 8000
+create chr-cms backend.chorknaben-biberach.de 8002
 
-echo "Generating config for staging site"
-export CHORSERV_IPS=$(dig +short chorserv-server-site-bundle-staging | sed 's/\(.*\)/server \1:8000;/')
-export CHORSERV_DOMAIN=staging.chorknaben-biberach.de
-envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/staging-site.conf
-
-echo "Generating config for staging backend"
-export CHORSERV_IPS=$(dig +short chr-cms-staging | sed 's/\(.*\)/server \1:8002;/')            
-export CHORSERV_DOMAIN=backend.staging.chorknaben-biberach.de
-envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/staging-backend.conf
+create chorserv-server-site-bundle-staging staging.chorknaben-biberach.de 8000
+create chr-cms-staging backend.staging.chorknaben-biberach.de 8002
 
 if pgrep -x "nginx" > /dev/null
 then
