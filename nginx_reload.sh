82b5973cebf7fab0e29b14f084587394d194a0b2
Georg Grab <talkdirty@users.noreply.github.com>
2017-02-23 18:44:29 +0100

    add staging env to nginx_reload.sh

--- nginx_reload.sh
+++ nginx_reload.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-export CHORSERV_PROD_IPS=$(dig +short chorserv-server-site-bundle | sed 's/\(.*\)/server \1:8000;/')
-export CHORSERV_PROD_BACKEND_IPS=$(dig +short chr-cms | sed 's/\(.*\)/server \1:8002;/')            
 
+echo "Generating config for production site"
+export CHORSERV_IPS=$(dig +short chorserv-server-site-bundle | sed 's/\(.*\)/server \1:8000;/')
+export CHORSERV_DOMAIN=chorknaben-biberach.de
 envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/production-site.conf
-envsubst < templates/production-backend.conf.tpl > /etc/nginx/conf.d/production-backend.conf
+
+echo "Generating config for production backend"
+export CHORSERV_IPS=$(dig +short chr-cms | sed 's/\(.*\)/server \1:8002;/')            
+export CHORSERV_DOMAIN=backend.chorknaben-biberach.de
+envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/production-backend.conf
+
+echo "Generating config for staging site"
+export CHORSERV_IPS=$(dig +short chorserv-server-site-bundle-staging | sed 's/\(.*\)/server \1:8000;/')
+export CHORSERV_DOMAIN=staging.chorknaben-biberach.de
+envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/staging-site.conf
+
+echo "Generating config for staging backend"
+export CHORSERV_IPS=$(dig +short chr-cms-staging | sed 's/\(.*\)/server \1:8002;/')            
+export CHORSERV_DOMAIN=backend.staging.chorknaben-biberach.de
+envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/staging-backend.conf
 
 if pgrep -x "nginx" > /dev/null
 then
