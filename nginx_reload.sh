987b0574cf5e9b37768aeed52b151c7821e81fde
Georg Grab <talkdirty@users.noreply.github.com>
2017-02-23 19:03:49 +0100

    deactivate backend

--- nginx_reload.sh
+++ nginx_reload.sh
@@ -4,6 +4,7 @@ function create {
 	echo "Generating config for (docker-compose name $1, domain $2, internal port $3)"
 	export CHORSERV_IPS=$(dig +short $1 | sed "s/\(.*\)/server \1:$3;/")
 	export CHORSERV_DOMAIN=$2
+	echo "got ips: $CHORSERV_IPS"
 	if [ ! -z "$CHORSERV_IPS" ]; then
 		envsubst < templates/production-site.conf.tpl > /etc/nginx/conf.d/$2.conf
 	else
@@ -12,10 +13,10 @@ function create {
 }
 
 create chorserv-server-site-bundle chorknaben-biberach.de 8000
-create chr-cms backend.chorknaben-biberach.de 8002
+#create chr-cms backend.chorknaben-biberach.de 8002
 
 create chorserv-server-site-bundle-staging staging.chorknaben-biberach.de 8000
-create chr-cms-staging backend.staging.chorknaben-biberach.de 8002
+#create chr-cms-staging backend.staging.chorknaben-biberach.de 8002
 
 if pgrep -x "nginx" > /dev/null
 then
